a9577069ff0e1e51741808d5547e5b1027b7e4cd
Oste Jannick <jannick@oste.dev>
2023-01-23 14:55:01 +0100

    hotfix push

--- Cpp/Cpp.ino
+++ Cpp/Cpp.ino
@@ -1,11 +1,6 @@
 /**
 * Arduino ino ew run file. (better not to write code here, all ino files get concatenated based on alphabatical order, this results in unwanted bugs sometimes.)
 *
-*  Required libraries: 
-* - https://github.com/espressif/esp32-camera
-* - https://github.com/me-no-dev/ESPAsyncWebServer
-* - https://github.com/me-no-dev/
-* - https://github.com/instanceofMA/arduino-fetch
 */
 #include <WiFi.h>
 #include "Application.h"
